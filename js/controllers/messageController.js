a73158ad12db9103d3e43e17f57ecfa23d3ad059
Romando Garcia <garcia.mando@yahoo.com>
2014-05-23 12:41:45 -0500

    angular functionality added

--- js/controllers/messageController.js
+++ js/controllers/messageController.js
@@ -2,23 +2,19 @@
 
 	var myApp = angular.module('messageSystem',[]);
 
-
 	myApp.controller('messageController', function(){
 
 		this.tab = 0;
 
-		this.setTab = function(tabVal){
+		this.tabSelected = function(tabVal){
 
 			this.tab = tabVal;
 		};
 
-		// this.isButtonVal = function(){
+		 this.isSelected = function(tabVal){
 
-		// 	if(this.send == 1)
-		// 		return true;
-		// 	else
-		// 		return false;
-		// };
+		 	return this.tab === tabVal;
+		};
 	});
 
 })();
