ad73db2ed1bf510f65c1ba1b0b616c7c83ececa6
tsoteen89 <t.osteen89@gmail.com>
2014-05-23 00:09:31 -0500

    making Post work

--- phpTest/testPost.php
+++ phpTest/testPost.php
@@ -23,18 +23,17 @@
 
 
     //$con = mysqli_connect("msu2u.net", "root", "rugger31", "Aii") or die("Server did not respond");
-    $con = mysqli_connect("localhost", "travis", "scooter", "aii") or die("Server did not respond");
+    $con = mysqli_connect("localhost", "root", "", "aii") or die("Server did not respond");
     mysqli_query($con,"INSERT INTO Patient(PatientID, fname, lname, mi, DOB, streetAddress, city, state, zip,
     Sex, Race, BMI, Height, Weight) VALUES ('$PatientID','$fname','$lname','$mi', '$DOB', '$streetAddress', '$city', '$state', '$zip',
     '$Sex', '$Race', '$BMI', '$Height', '$Weight')");
 
     echo "1 record was inserted \n";
-    echo "Record: ". $_POST['list.fname'];
 
 
 
   mysqli_close($con);
-
+/*
   echo "Pat ID:".$_POST["PatientID"];
   echo "First: ".$_POST["fname"];
   echo "Last: ".$_POST["lname"];
@@ -49,10 +48,43 @@
   echo "BMI: ".$_POST["BMI"];
   echo "Height: ".$_POST["Height"];
   echo "Weight: ".$_POST["Weight"];
+*/
 
 
 
 
-  //echo "First:".$_POST["newOb[0].fname"];
+/*
+    echo $_POST['name'];
+    echo $_POST['superheroAlias'];
+
+
+$errors         = array();  	// array to hold validation errors
+$data 			= array(); 		// array to pass back data
+
+// validate the variables ======================================================
+	if (empty($_POST['name']))
+		$errors['name'] = 'Name is required.';
+
+	if (empty($_POST['superheroAlias']))
+		$errors['superheroAlias'] = 'Superhero alias is required.';
+
+// return a response ===========================================================
+
+	// response if there are errors
+	if ( ! empty($errors)) {
+
+		// if there are items in our errors array, return those errors
+		$data['success'] = false;
+		$data['errors']  = $errors;
+	} else {
+
+		// if there are no errors, return a message
+		$data['success'] = true;
+		$data['message'] = 'Success!';
+	}
+
+	// return all our data to an AJAX call
+	echo json_encode($data);
+*/
 
 ?>
