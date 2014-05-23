2e9a611bea23e8e6140dc0ad1f95282e595e10b7
tsoteen89 <t.osteen89@gmail.com>
2014-05-23 00:09:31 -0500

    making Post work

--- js/controllers/basicCtrl.js
+++ js/controllers/basicCtrl.js
@@ -1,5 +1,5 @@
-(function() {
-	 var myApp = angular.module('basicCtrl', []);
+
+var myApp = angular.module('basicCtrl', []);
 
 var controllers = {};
 
@@ -96,7 +96,53 @@ controllers.newPatientsController = function ($scope) {
 				});*/
 	};
 };
+    
+    
+    
+    
+controllers.formController = function($scope, $http) {
+    
+    $scope.bmis = [];
+	$scope.heights = [];
+	$scope.weights = [];
+	$scope.i = 0;
+
+	for($scope.i=0;$scope.i<36;$scope.i++){
+		$scope.bmis[$scope.i] = $scope.i + 10;
+	}
+
+	for($scope.i=0;$scope.i<68;$scope.i++){
+		$scope.heights[$scope.i] = $scope.i + 12;
+	}
+
+	for($scope.i=0;$scope.i<221;$scope.i++){
+		$scope.weights[$scope.i] = $scope.i + 80;
+	}
+
+    // create a blank object to hold our form information
+    // $scope will allow this to pass between controller and view
+    $scope.formData = {};
+
+    // process the form
+    $scope.processForm = function() {
+        $http({
+            method  : 'POST',
+            url     : 'phpTest/testPost.php',
+            data    : $.param($scope.formData),  // pass in data as strings
+            headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  
+        })
+            .success(function(data) {
+                console.log(data);
+            });
+
+    };
+
+}
+    
+    
+    
+    
 
 
 myApp.controller(controllers);
-})();
+
