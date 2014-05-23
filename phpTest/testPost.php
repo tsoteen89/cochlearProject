a1b2abee9af34c7fba899fb1cc98bb229401e988
tsoteen89 <fapppo@traviss-air.att.net>
2014-05-22 20:37:26 -0500

    File Structure Changes and New Patient Form working

--- phpTest/testPost.php
+++ phpTest/testPost.php
@@ -5,25 +5,26 @@
       header('Access-Control-Allow-Credentials: true');
       header('Access-Control-Max-Age: 86400');    // cache for 1 day
   }
-/*
-  $PatientID = $_POST['list.PatientID'];
-  $fname = $_POST['list.fname'];
-  $lname = $_POST['list.lname'];
-  $mi = $_POST['list.mi'];
-  $DOB = $_POST['list.DOB'];
-  $streetAddress = $_POST['list.streetAddress'];
-  $city = $_POST['list.city'];
-  $state = $_POST['list.state'];
-  $zip = $_POST['list.zip'];
-  $Sex = $_POST['list.Sex'];
-  $Race = $_POST['list.Race'];
-  $BMI = $_POST['list.BMI'];
-  $Height = $_POST['list.Height'];
-  $Weight = $_POST['list.Weight'];
+
+  $PatientID = $_POST['PatientID'];
+  $fname = $_POST['fname'];
+  $lname = $_POST['lname'];
+  $mi = $_POST['mi'];
+  $DOB = $_POST['DOB'];
+  $streetAddress = $_POST['streetAddress'];
+  $city = $_POST['city'];
+  $state = $_POST['state'];
+  $zip = $_POST['zip'];
+  $Sex = $_POST['Sex'];
+  $Race = $_POST['Race'];
+  $BMI = $_POST['BMI'];
+  $Height = $_POST['Height'];
+  $Weight = $_POST['Weight'];
 
 
-    $con = mysqli_connect("msu2u.net", "root", "rugger31", "Aii") or die("Server did not respond");
-    mysqli_query($con,"INSERT INTO patient(PatientID, fname, lname, mi, DOB, streetAddress, city, state, zip,
+    //$con = mysqli_connect("msu2u.net", "root", "rugger31", "Aii") or die("Server did not respond");
+    $con = mysqli_connect("localhost", "travis", "scooter", "aii") or die("Server did not respond");
+    mysqli_query($con,"INSERT INTO Patient(PatientID, fname, lname, mi, DOB, streetAddress, city, state, zip,
     Sex, Race, BMI, Height, Weight) VALUES ('$PatientID','$fname','$lname','$mi', '$DOB', '$streetAddress', '$city', '$state', '$zip',
     '$Sex', '$Race', '$BMI', '$Height', '$Weight')");
 
@@ -49,9 +50,9 @@
   echo "Height: ".$_POST["Height"];
   echo "Weight: ".$_POST["Weight"];
 
-*/
 
 
-  echo "First:".$_POST["newOb[0].fname"];
+
+  //echo "First:".$_POST["newOb[0].fname"];
 
 ?>
