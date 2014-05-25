ef3a5a0cf292461348e8161c62aed9677d327c62
Jokii19 <jokii19>
2014-05-24 21:07:43 -0500

    insertion of patient data files fixed

--- careTeam/js/careTeamControllers/ct_insertpatientcontrollers.js
+++ careTeam/js/careTeamControllers/ct_insertpatientcontrollers.js
@@ -4,6 +4,20 @@ var controllers={};
 
 controllers.FrmController = function($scope, $http){
     
+    $scope.treatments = [];
+    $scope.i = 0;
+    
+    
+    //Populating the treatment duration array
+    for($scope.i = 0; $scope.i < 24; $scope.i++){
+        
+        $scope.treatments[$scope.i] = $scope.i+1;
+        
+    }
+    
+    
+    
+    
     $scope.formData = {};
     
     $scope.processForm = function() {
