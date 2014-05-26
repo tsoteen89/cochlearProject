800c8d954c6e761bb5d83661a9c7a9bf3c485c05
annevu <anne.v.lam@gmail.com>
2014-05-25 22:40:50 -0500

    edit nonsense

--- phpTest/getData.php
+++ phpTest/getData.php
@@ -7,11 +7,8 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
 }
 
-<<<<<<< HEAD
 $con = mysqli_connect("localhost", "root", "","aii") or die("Server did not respond");
-=======
-$con = mysqli_connect("msu2u.net", "root", "rugger31","aii") or die("Server did not respond");
->>>>>>> origin/annes_branch
+//$con = mysqli_connect("msu2u.net", "root", "rugger31","Aii") or die("Server did not respond");
 $result = mysqli_query($con,"SELECT * FROM patient");
 
 while($row = mysqli_fetch_assoc($result))
---
+++ phpTest/update.php
@@ -0,0 +1,31 @@
+<?php
+
+echo $_POST["street"];
+echo $_POST["city"];
+echo $_POST["state"];
+echo $_POST["zip"]; 
+echo $_POST["id"];
+
+$street = $_POST["street"];
+$newCity = $_POST["city"];
+$newState = $_POST["state"];
+$newZip = $_POST["zip"];
+$id= $_POST["id"];
+
+$conn = mysqli_connect("localhost", "root", "","aii") or die("Server did not respond");
+if(! $conn )
+
+// HMM.. DOO SOMETHING WITH THiSS
+$sql = 'UPDATE patient
+        SET streetAddress=street, city=newCity, state=newState, zip=newZip,
+        WHERE PatientID=id';
+
+mysql_select_db('TUTORIALS');
+$retval = mysql_query( $sql, $conn );
+if(! $retval )
+{
+  die('Could not update data: ' . mysql_error());
+}
+echo "Updated data successfully\n";
+mysql_close($conn);
+?>
