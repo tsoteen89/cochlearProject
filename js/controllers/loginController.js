3d59137e9116f29e05818f958d70dfecf042a3ee
James55 <jrrowe55@yahoo.com>
2014-05-28 03:59:43 -0500

    Login Controller

--- js/controllers/loginController.js
+++ js/controllers/loginController.js
@@ -1,13 +1,32 @@
-(function(){var myApp = angular.module('users.controller', []);
+(function(){
+
+var login = angular.module('login', []);
+
 
 var controllers = {};
 
-//Comment!!!
+controllers.LoginController = function($scope){
+	$scope.username = "";
+	$scope.password = "";
+	$scope.test = "blah";
+}
+
 controllers.loginControl = function ($scope){
 	$scope.open = function($event){
 		$event.preventDefault();
 		$event.stopPropagation();
-	}
+		
+		$scope.opened = true;
+	};
+	
+	$scope.username = "";
+	$scope.password = "";
+	$scope.list = [];
+	
+	$scope.submit = function(){
+		$scope.list.push({'username':this.username, 'password':this.password});
+	};
 }
 
+myApp.controller(controllers);
 })();
