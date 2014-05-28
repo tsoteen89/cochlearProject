ec21c9be1a6d97f1c7234ed32eae1f57ade727bc
James Rowe <jrrowe55@yahoo.com>
2014-05-28 13:45:44 -0500

    Added bare bones register controller

--- js/controllers/registerController.js
+++ js/controllers/registerController.js
@@ -1,1 +1,23 @@
-{\rtf1}
+(function(){
+
+var register = angular.module('register', []);
+
+var controllers = {};
+
+controllers.regControl = function ($scope){
+		$scope.open = function($event){
+		$event.preventDefault();
+		$event.stopPropagation();
+			
+		$scope.opened = true;
+	};
+	
+	$scope.register = {};
+	
+	$scope.submit = function(){
+		//$scope.list.push({'username':this.username, 'password':this.password});
+	};
+}
+
+register.controller(controllers);
+})();
