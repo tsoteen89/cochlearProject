7a79aadf5ac1c417baf6d87c34b2c0600816cdcf
tsoteen89 <fapppo@Traviss-MacBook-Air.local>
2014-05-28 13:25:27 -0500

    changes made to Get and Post in newPatient

--- js/controllers/basicCtrl.js
+++ js/controllers/basicCtrl.js
@@ -55,12 +55,42 @@ controllers.formController = function($scope, $http) {
             data    : $.param($scope.formData),  // pass in data as strings
             headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  
         })
-            .success(function(data) {
-                console.log(data);
-            });
+        .success(function(data) {
+            console.log(data);
+        });
 
     };
 
+}
+
+
+controllers.newAPICtrl = function($scope, $http, $templateCache)
+{
+    $scope.theVar = 4;
+    
+    $scope.method = 'GET';
+    $scope.url = 'http://api.msu2u.net/v1/patient/';
+
+    $scope.fetch = function() {
+      $scope.code = null;
+      $scope.response = null;
+
+      $http({
+          method: $scope.method, 
+          url: $scope.url, 
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          cache: $templateCache}
+      ).
+      success(function(data, status) {
+          $scope.status = status;
+          $scope.data = data;
+      }).
+      error(function(data, status) {
+      $scope.data = data || "Request failed";
+      $scope.status = status;
+      });
+
+}
 }
 
 
