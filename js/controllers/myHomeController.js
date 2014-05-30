af95aa1d5845c1fb3662dce438222507eafaedb5
James Rowe <jrrowe55@yahoo.com>
2014-05-30 11:03:03 -0500

    slightly modified myHomeController

--- js/controllers/myHomeController.js
+++ js/controllers/myHomeController.js
@@ -15,9 +15,8 @@ controllers.homeController = function ($scope){
 	
     var test = 'howdy';
     
-    $scope.alerts = [
-        { content: 'John Smith: Care phase has changed to Preoperative Visit'}
-    ];
+    $scope.alerts = 
+        { content: 'John Smith: Care phase has changed to Preoperative Visit'};
     
     $scope.notifications = [{content: 'Dr. Charlie Bravo has accepted your invitation to John Smith\'s care team'},
                          {content: 'Dr. Sierra Victor has invited you to join Jane Doe\'s care team'}];
@@ -32,10 +31,6 @@ controllers.homeController = function ($scope){
             subject: 'Could I send Jane Doe your way for candidacy testing?',
             content: '.......'
         }];
-    
-	$scope.submit = function(){
-		$scope.list.push({'username':this.username, 'password':this.password});
-	};
 }
 
 myApp.controller(controllers);
