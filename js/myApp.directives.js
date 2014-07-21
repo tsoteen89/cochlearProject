2170456daf73c53161e03380f58a78e652b7d695
tsoteen89 <t.osteen89@gmail.com>
2014-07-21 09:31:04 -0500

    Cleaning out the site

--- js/myApp.directives.js
+++ js/myApp.directives.js
@@ -2,62 +2,6 @@
 
 var myApp = angular.module('myApp.directives', []);
 
-myApp.directive('newPatientForm', function() {
-	return {
-		restrict: 'E',
-		templateUrl: 'directives/new-patient-form.html'
-	}
-});	
-
-myApp.directive('editPatientContact', function() {
-	return {
-		restrict: 'E',
-		templateUrl: 'directives/edit-patient-contact.html'
-	}
-});	
-
-myApp.directive('periopTab', function() {
-	return {
-		restrict: 'E',
-		templateUrl: 'directives/periop-tab.html'
-	}
-});	
-
-myApp.directive('candidacyTab', function() {
-	return {
-		restrict: 'E',
-		templateUrl: 'directives/candidacy-tab.html'
-	}
-});	
-
-myApp.directive('audiometricTesting', function() {
-	return {
-		restrict: 'E',
-		templateUrl: 'directives/audiometric-testing.html'
-	}
-});
-
-myApp.directive('demographicsTab', function() {
-	return {
-		restrict: 'E',
-		templateUrl: 'directives/demographics-tab.html'
-	}
-});
-
-myApp.directive('surgicalConsultTab', function() {
-	return {
-		restrict: 'E',
-		templateUrl: 'directives/surgical-consult-tab.html'
-	}
-});
-
-myApp.directive('oneWeekTab', function() {
-	return {
-		restrict: 'E',
-		templateUrl: 'directives/one-week-tab.html'
-	}
-});
-
 var INTEGER_REGEXP = /^\-?\d+$/;
 myApp.directive('integer', function() {
     
