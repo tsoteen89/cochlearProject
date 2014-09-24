95d0eb7e1bdfab967711151294493c56f9b23c8d
rugbyprof <terry.griffin@mwsu.edu>
2014-09-23 22:55:56 -0500

    audiogram awesomness

--- audiogram/test.php
+++ audiogram/test.php
@@ -1,4 +1,18 @@
-<?php
+<!DOCTYPE HTML>
+<html>
+  <head>
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <style>
+      body {
+        margin: 0px;
+        padding: 0px;
+      }
+    </style>
+  </head>
+  <body>
+    <canvas id="myCanvas" width="600" height="600"></canvas>
+    <script>
+    </script>
 
-$data = print_r($_POST,true);
-file_put_contents("data.txt",$data);
+  </body>
+</html>
