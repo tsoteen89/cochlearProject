9bd947c9f22f885c50a60e19a6e62c581ad554ac
rugbyprof <terry.griffin@mwsu.edu>
2014-09-27 01:20:11 -0500

    fixed margin issues

--- audiogram/audiogram_module2.js
+++ audiogram/audiogram_module2.js
@@ -2,23 +2,19 @@ var AudioGram = function(element_id, audiogram_id,side) {
     // Private data
     var imageObj = new Image();
     var private = {
+        font_size     : 16,
         element_id    : element_id,
         audiogram_id  : audiogram_id,
         side          : side,
-        i             : 0,
-        x             : 0,
-        y             : 0,
         canvas        : null,           //Future reference to html element
         ctx           : null,           //Future canvas reference
         canvas_width  : 0,              //Width of canvas in pixels
         canvas_height : 0,              //Height of canvas in pixels
         column_width  : 0,		        //width of a column function based on width of canvas
         row_height    : 0,              //height of a row function based on height of canvas
-        canvas_buffer : 0,		        //padding in pixels?
-        margin_top    : 20,
-        margin_left   : 10,
-        margin_right  : 0,
-        margin_bottom : 20,
+        margins       : {"top":40,"bottom":20,"left":40,"right":20},
+        graph_bounds  : {"min":{"x":0,"y":0},"max":{"x":0,"y":0}},
+        graph_size    : {"width":0,"height":0},
         crnt_measure  : null,
         masked        : 'unmasked',     //Masked = masked Unmasked = unmasked :)
         x_labels      : [],             //Values displayed on top of audiogram
@@ -113,11 +109,21 @@ var AudioGram = function(element_id, audiogram_id,side) {
             }
             
             //Load extra X values because we want more possibilities than just the listed frequencies.
-            this.x_values = [125,250,500,750,1000,1500,2000,3000,4000,6000,8000];
-        },
-        calcRowColSize: function() {
-            this.column_width = (this.canvas_width - (this.margin_left + this.margin_right)) / (this.x_labels.length + 1);
-            this.row_height = (this.canvas_height - (this.margin_top + this.margin_bottom)) / (this.y_labels.length + 1);
+            this.x_values = [125,186,250,375,500,750,1000,1500,2000,3000,4000,6000,8000];
+            
+            this.graph_bounds['min']['x'] = this.margins["left"];
+            this.graph_bounds['min']['y'] = this.margins["top"];
+            this.graph_bounds['max']['x'] = this.canvas_width - this.margins["right"];
+            this.graph_bounds['max']['y'] = this.canvas_height - this.margins["bottom"];
+            this.graph_size["width"] = this.canvas_width-(this.margins["left"]+this.margins["right"]);
+            this.graph_size["height"] = this.canvas_height-(this.margins["top"]+this.margins["bottom"]);
+            
+            //calculate row width and column height
+            this.column_width = Math.floor((this.graph_bounds['max']['x'] - this.graph_bounds['min']['x']) / (this.x_labels.length-1));
+            this.row_height = Math.floor((this.graph_bounds['max']['y'] - this.graph_bounds['min']['y']) / (this.y_labels.length-1));
+            
+            console.log(this.graph_bounds,this.graph_size,this.margins);
+            console.log(this.row_height,this.column_width);
         },
         drawOuterBorder: function() {
             this.ctx.rect(0, 0, this.canvas_width, this.canvas_height);      
@@ -125,21 +131,40 @@ var AudioGram = function(element_id, audiogram_id,side) {
             this.ctx.strokeStyle = 'black';
             this.ctx.stroke();
         },
-        addFrequencyLabels: function(){
-            for(this.i=0,this.x=this.margin_left+this.column_width;this.i<this.x_labels.length;this.i++,this.x+=this.column_width){
-                this.ctx.fillText(this.x_labels[this.i], this.x-10, this.margin_top*2);
-                this.ctx.beginPath();
-                this.ctx.moveTo(this.x, this.margin_top+this.row_height);
-                this.ctx.lineTo(this.x, this.canvas_height-this.row_height-this.margin_bottom);
+        addLabels: function(){
+            var i;
+            var x;
+            var y;
+            for(i=0,x=this.graph_bounds["min"]["x"];i<this.x_labels.length;i++,x+=this.column_width){
+                this.ctx.fillText(this.x_labels[i],x-10,this.graph_bounds["min"]["y"]-10);
+                this.ctx.stroke();
+            }
+            
+            for(i=0,y=this.graph_bounds["min"]["y"];i<this.y_labels.length;i++,y+=this.row_height){
+                this.ctx.fillText(this.y_labels[i],this.graph_bounds["min"]["x"]-30,y+5);
                 this.ctx.stroke();
             }
         },
-        addDBLabels: function(){
-            for(this.i=0,this.y=this.margin_top+this.row_height;this.i<this.y_labels.length;this.i++,this.y+=this.row_height){
-                this.ctx.fillText(this.y_labels[this.i], this.margin_left, this.y+5);
+        drawGraph: function(){
+            var x;
+            var y;
+            var i;
+            
+            this.ctx.rect(this.graph_bounds["min"]["x"],this.graph_bounds["min"]["y"],this.graph_size["width"],this.graph_size["height"]);      
+            this.ctx.lineWidth = 1;
+            this.ctx.strokeStyle = 'black';
+            this.ctx.stroke();  
+            
+            for(i=0,x=this.graph_bounds["min"]["x"];i<this.x_labels.length-1;i++,x+=this.column_width){
                 this.ctx.beginPath();
-                this.ctx.moveTo(this.margin_left+this.column_width,this.y);
-                this.ctx.lineTo(this.canvas_width-this.column_width,this.y);
+                this.ctx.moveTo(x,this.graph_bounds["min"]["y"]);
+                this.ctx.lineTo(x,this.graph_bounds["max"]["y"]);
+                this.ctx.stroke();
+            }
+            for(i=0,y=this.graph_bounds["min"]["y"];i<this.y_labels.length-1;i++,y+=this.row_height){
+                this.ctx.beginPath();
+                this.ctx.moveTo(this.graph_bounds["min"]["x"],y);
+                this.ctx.lineTo(this.graph_bounds["max"]["x"],y);
                 this.ctx.stroke();
             }
         },
@@ -151,76 +176,90 @@ var AudioGram = function(element_id, audiogram_id,side) {
             };
         },
         getFrequency: function(x){
-            //Find the greatest "x" value that should be clicked
-            var max_x = this.column_width*(this.x_labels.length-1);
+            var d;
+            var r;
+            x = x - this.graph_bounds["min"]["x"];              //adjust x because of margins   
+            d = Math.floor(x / (this.column_width / 2));        //How many 1/2 columns divide into x
             
-            //Adjust the "x" value making it zero for a click on the far left column 
-            x -= this.margin_left+this.column_width;
+            r = x % (this.column_width / 2);
             
-            //Now just find a new x based on a percentage of the distance from 
-            //the far left. Use this x as the index into the array.
-            //E.g. 50% from far left gives us the "middle" value in the array
-            //essentially snapping us to discrete values.
-            x = Math.round(x / max_x * (this.x_values.length-1));
-            
-            return this.x_values[x];
-        },
-        getDecibels: function(y){
-            //Find the greatest "y" value that should be clicked
-            var max_y = this.row_height*(this.y_labels.length)-this.margin_top;
-            
-            //Adjust the "y" value making it zero for a click on the top of the graph 
-            y -= this.margin_top+this.row_height;
-            
-            //Now just find a new x based on a percentage of the distance from 
-            //the far left. Use this x as the index into the array.
-            //E.g. 50% from far left gives us the "middle" value in the array
-            //essentially snapping us to discrete values.
-            y = Math.round(y / max_y * (this.y_values.length-1));
-            return this.y_values[y];
-        },
-        snapClick: function(x,y){
-            this.ctx.fillStyle = 'green';
-            this.ctx.font = '16pt helvetica';
-            //this.ctx.fillText(String.fromCharCode(parseInt(0x2022), 16),x-12,y+10); 
-            
-            var max_x = this.column_width*(this.x_labels.length)+3;
-            var min_x = this.margin_left+this.column_width-3;
-            var max_y = this.canvas_height-this.row_height-this.margin_bottom;     //Hack Hack
-            var min_y = this.margin_top+this.row_height+6;                              //Hack Hack
-
-            this.ctx.fillText(String.fromCharCode(parseInt(0x25A0), 16),x,min_y); 
-            this.ctx.fillText(String.fromCharCode(parseInt(0x25A0), 16),x,max_y);     
-            this.ctx.fillText(String.fromCharCode(parseInt(0x25A0), 16),max_x,y);  
-            this.ctx.fillText(String.fromCharCode(parseInt(0x25A0), 16),min_x,y); 
-            
-        },
-        addTxtMeasure: function(x,y){
-            var font_size = 18;
-            this.ctx.font = font_size+'pt helvetica';
-            this.ctx.fillStyle = 'red';
-            if(side=='right'){
-                this.ctx.fillStyle = 'red';
-                font_size = 18;
-                this.ctx.font = font_size+'pt helvetica';
-                //this.ctx.fillText(String.fromCharCode(parseInt(0x25CB), 16),x-10,y+10);
-                this.ctx.fillText(this.current_char,x-10,y+10);
+            if(r/(this.column_width / 2) > .5){
+                d = d + 1;
+            }
+                        
+            //This hot mess is because:
+            //Labels =              125,250,500,1000,2000,4000,8000
+            //Possible Returns =    125,250,375,500,1000,1500,2000,3000,6000,8000
+            //So there's a values between every label except <500. Snapping to the
+            //nearest frequency changes for values below 500.
+            if(d>=4){                                         
+                return this.x_values[d];
             }else{
-                font_size = 18;
-                this.ctx.fillStyle = 'blue';
-                this.ctx.font = font_size+'pt helvetica';
-                //this.ctx.fillText(String.fromCharCode(parseInt(0x0FBE), 16),x-12,y+10); 
-                this.ctx.fillText(this.current_char,x-10,y+10);
+                r = x % this.column_width;
+                r /= this.column_width
+
+                if(d == 3 && r >.50)
+                    return this.x_values[4]                    //Hacky
+                else if(d == 0)
+                    return this.x_values[0];
+                else if(d == 1 && r < .50)
+                    return this.x_values[0]
+                else
+                    return this.x_values[2]                    
+            
             }
         },
-        addMeasure: function(x,y){
-            console.log(x,y);
-            this.audiogram_vals.push({'measure':this.crnt_measure,'x':x,'y':y,'freq':this.getFrequency(x),'dB':this.getDecibels(y),'symbol':this.measureImageObj[this.crnt_measure][this.masked][this.side]});           
-            this.snapClick(x,y);
-            //imageObj.src = this.current_char;
-            //this.ctx.drawImage(imageObj, x-12, y-12);
-            //console.log("adj x,y ="+(x-12)+" "+(y-12));
-            //console.log(this.audiogram_vals);
+        getDecibels: function(y){
+            var d;
+            var r;
+            y = y - this.graph_bounds["min"]["y"];              //adjust y because of margins    
+            d = Math.floor(y / (this.row_height / 2));          //How many 1/2 rows divide into y
+            r = y % (this.row_height / 2);                      //Remainder (how close is it to the next value).
+            
+            if(r/(this.row_height / 2) > .5)
+                d = d + 1;
+            return this.y_values[d];
+        },
+        snapClick: function(x,y){
+            var d;      //Number of times divided into
+            var r;      //Remainder after division
+            var snap_x; //snapped to x value
+            var snap_y; //snapped to y value
+
+            x = x - this.graph_bounds["min"]["x"];              //adjust x because of margins   
+            d = Math.floor(x / (this.column_width / 2));        //How many 1/2 columns divide into x
+            r = x % (this.column_width / 2);                    //Remainder (how close is it to the next value).
+            
+            if(r/(this.column_width / 2) > .5)
+                d = d + 1;
+            
+            snap_x = d * (this.column_width / 2) + this.graph_bounds["min"]["x"];//Multiply d by 1/2 column size to snap to an edge
+            
+            y = y - this.graph_bounds["min"]["y"];              //adjust y because of margins    
+            d = Math.floor(y / (this.row_height / 2));          //How many 1/2 rows divide into y
+            r = y % (this.row_height / 2);                      //Remainder (how close is it to the next value).
+            
+            if(r/(this.row_height / 2) > .5)
+                d = d + 1;
+            
+            snap_y = d * (this.row_height / 2) + this.graph_bounds["min"]["y"];//Multiply d by 1/2 column size to snap to an edge
+            
+            return {"x":snap_x,"y":snap_y};
+            
+        },
+        addMeasure: function(x,y,callback){
+            var snap = this.snapClick(x,y);
+            this.audiogram_vals.push({'measure':this.crnt_measure,'x':snap['x'],'y':snap['y'],'freq':this.getFrequency(x),'dB':this.getDecibels(y),'symbol':this.measureImageObj[this.crnt_measure][this.masked][this.side]});           
+            this.audiogram_vals = callback(this.audiogram_vals,'x');
+        },
+        printMeasures: function(){
+            var i;
+            this.ctx.beginPath();
+            for(i=0;i<this.audiogram_vals.length-1;i++){
+                this.ctx.moveTo(this.audiogram_vals[i]['x'],this.audiogram_vals[i]['y']);
+                this.ctx.lineTo(this.audiogram_vals[i+1]['x'],this.audiogram_vals[i+1]['y']);               
+            }
+            this.ctx.stroke(); 
         },
         setTxtCharacter: function(measure){
             this.crnt_measure = measure;
@@ -249,26 +288,6 @@ var AudioGram = function(element_id, audiogram_id,side) {
         },
         //sortJsonArrayByProperty(results, 'attributes.OBJECTID');
         //sortJsonArrayByProperty(results, 'attributes.OBJECTID', -1);
-        sortJsonArrayByProperty : function(objArray, prop, direction){
-            if (arguments.length<2) throw new Error("sortJsonArrayByProp requires 2 arguments");
-            var direct = arguments.length>2 ? arguments[2] : 1; //Default to ascending
-
-            if (objArray && objArray.constructor===Array){
-                var propPath = (prop.constructor===Array) ? prop : prop.split(".");
-                objArray.sort(function(a,b){
-                    for (var p in propPath){
-                        if (a[propPath[p]] && b[propPath[p]]){
-                            a = a[propPath[p]];
-                            b = b[propPath[p]];
-                        }
-                    }
-                    // convert numeric strings to integers
-                    a = a.match(/^\d+$/) ? +a : a;
-                    b = b.match(/^\d+$/) ? +b : b;
-                    return ( (a < b) ? -1*direct : ((a > b) ? 1*direct : 0) );
-                });
-            }
-        }
     }
     private['canvas'] = document.getElementById(private['element_id']);
     private['ctx'] = private['canvas'].getContext('2d');
@@ -279,19 +298,15 @@ var AudioGram = function(element_id, audiogram_id,side) {
         var mousePos = private['getMousePos'](private['canvas'], evt);
         mousePos.x = Math.floor(mousePos.x);
         mousePos.y = Math.floor(mousePos.y);
-        private['getFrequency'](mousePos.x);
-        private['getDecibels'](mousePos.y);
-        private['addMeasure'](mousePos.x,mousePos.y);      
+        private['addMeasure'](mousePos.x,mousePos.y,MySort);
+        private['printMeasures']();
+        console.log(private['audiogram_vals']);
         
     }, false);
     private['initArrays']();
-    private['calcRowColSize']();
     private['drawOuterBorder']();
-    private['addFrequencyLabels']();
-    private['addDBLabels']();
-    console.log(private['x_values'],private['y_values']);
-    //private['tempLoadMeasures']();
-    //private['printMeasures']();
+    private['addLabels']();
+    private['drawGraph']();
 
     // Expose public API
     return {
@@ -308,3 +323,45 @@ var AudioGram = function(element_id, audiogram_id,side) {
         }
     }
 };
+
+//Bubble sort my audiogram array to "order"
+//the 'x' values for printing and connecting 
+//with a line.
+var MySort = function bubbleSort(a,prop)
+{
+    var swapped;
+    do {
+        swapped = false;
+        for (var i=0; i < a.length-1; i++) {
+            if (a[i]['x'] > a[i+1]['x']) {
+                var temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return a;
+}
+ 
+//var MySort = function sortJsonArrayByProperty(objArray, prop, direction){
+//    if (arguments.length<2) throw new Error("sortJsonArrayByProp requires 2 arguments");
+//    var direct = arguments.length>2 ? arguments[2] : 1; //Default to ascending
+//
+//    if (objArray && objArray.constructor===Array){
+//        var propPath = (prop.constructor===Array) ? prop : prop.split(".");
+//        objArray.sort(function(a,b){
+//            for (var p in propPath){
+//                if (a[propPath[p]] && b[propPath[p]]){
+//                    a = a[propPath[p]];
+//                    b = b[propPath[p]];
+//                }
+//            }
+//            console.log(typeof(a),typeof(b));
+//            // convert numeric strings to integers
+//            a = a.match(/^\d+$/) ? +a : a;
+//            b = b.match(/^\d+$/) ? +b : b;
+//            return ( (a < b) ? -1*direct : ((a > b) ? 1*direct : 0) );
+//        });
+//    }
+//}
