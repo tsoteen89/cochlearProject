825b9a60fb9db95aac3b9494268ad74a6cb2c9df
annevu <anne.v.lam@gmail.com>
2014-10-22 13:53:43 -0500

    facility report work

--- js/controllers/reportController.js
+++ js/controllers/reportController.js
@@ -11,10 +11,16 @@ controllers.reportCtrl = function($scope, getData, $cookieStore) {
 	var cookieSessionID = $cookieStore.get('SessionID');
     console.log(cookieSessionID);
     
+     $scope.isArray = function(check){
+        return angular.isArray(check);
+    }
+     
     $scope.facilityName = $cookieStore.get('FacilityName');
     var fullFacilityURL = '../aii-api/v1/reports/fullFacility/'+ cookieSessionID; 
     getData.get(fullFacilityURL).success(function(data) {
         $scope.facilityReport = data.records;
+    }).then(function(){
+        $scope.patientNum = Object.keys($scope.facilityReport).length;
     });
     
     var questionsURL = '../aii-api/v1/reports/allQuestions'; 
