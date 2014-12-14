f6064d7e0b80aa1568637a27a11d0d3f7e72ae5b
annevu <anne.v.lam@gmail.com>
2014-12-13 18:32:16 -0600

    commenting and indenting- general cleanup

--- js/controllers/aiiController.js
+++ js/controllers/aiiController.js
@@ -2263,6 +2263,65 @@
                 };
             });
         };
+        
+        $scope.usStates = [
+			{ name: 'ALABAMA', abbreviation: 'AL'},
+			{ name: 'ALASKA', abbreviation: 'AK'},
+			{ name: 'ARIZONA', abbreviation: 'AZ'},
+			{ name: 'ARKANSAS', abbreviation: 'AR'},
+			{ name: 'CALIFORNIA', abbreviation: 'CA'},
+			{ name: 'COLORADO', abbreviation: 'CO'},
+			{ name: 'CONNECTICUT', abbreviation: 'CT'},
+			{ name: 'DELAWARE', abbreviation: 'DE'},
+			{ name: 'DISTRICT OF COLUMBIA', abbreviation: 'DC'},
+			{ name: 'FLORIDA', abbreviation: 'FL'},
+			{ name: 'GEORGIA', abbreviation: 'GA'},
+			{ name: 'GUAM', abbreviation: 'GU'},
+			{ name: 'HAWAII', abbreviation: 'HI'},
+			{ name: 'IDAHO', abbreviation: 'ID'},
+			{ name: 'ILLINOIS', abbreviation: 'IL'},
+			{ name: 'INDIANA', abbreviation: 'IN'},
+			{ name: 'IOWA', abbreviation: 'IA'},
+			{ name: 'KANSAS', abbreviation: 'KS'},
+			{ name: 'KENTUCKY', abbreviation: 'KY'},
+			{ name: 'LOUISIANA', abbreviation: 'LA'},
+			{ name: 'MAINE', abbreviation: 'ME'},
+			{ name: 'MARSHALL ISLANDS', abbreviation: 'MH'},
+			{ name: 'MARYLAND', abbreviation: 'MD'},
+			{ name: 'MASSACHUSETTS', abbreviation: 'MA'},
+			{ name: 'MICHIGAN', abbreviation: 'MI'},
+			{ name: 'MINNESOTA', abbreviation: 'MN'},
+			{ name: 'MISSISSIPPI', abbreviation: 'MS'},
+			{ name: 'MISSOURI', abbreviation: 'MO'},
+			{ name: 'MONTANA', abbreviation: 'MT'},
+			{ name: 'NEBRASKA', abbreviation: 'NE'},
+			{ name: 'NEVADA', abbreviation: 'NV'},
+			{ name: 'NEW HAMPSHIRE', abbreviation: 'NH'},
+			{ name: 'NEW JERSEY', abbreviation: 'NJ'},
+			{ name: 'NEW MEXICO', abbreviation: 'NM'},
+			{ name: 'NEW YORK', abbreviation: 'NY'},
+			{ name: 'NORTH CAROLINA', abbreviation: 'NC'},
+			{ name: 'NORTH DAKOTA', abbreviation: 'ND'},
+			{ name: 'OHIO', abbreviation: 'OH'},
+			{ name: 'OKLAHOMA', abbreviation: 'OK'},
+			{ name: 'OREGON', abbreviation: 'OR'},
+			{ name: 'PALAU', abbreviation: 'PW'},
+			{ name: 'PENNSYLVANIA', abbreviation: 'PA'},
+			{ name: 'PUERTO RICO', abbreviation: 'PR'},
+			{ name: 'RHODE ISLAND', abbreviation: 'RI'},
+			{ name: 'SOUTH CAROLINA', abbreviation: 'SC'},
+			{ name: 'SOUTH DAKOTA', abbreviation: 'SD'},
+			{ name: 'TENNESSEE', abbreviation: 'TN'},
+			{ name: 'TEXAS', abbreviation: 'TX'},
+			{ name: 'UTAH', abbreviation: 'UT'},
+			{ name: 'VERMONT', abbreviation: 'VT'},
+			{ name: 'VIRGIN ISLANDS', abbreviation: 'VI'},
+			{ name: 'VIRGINIA', abbreviation: 'VA'},
+			{ name: 'WASHINGTON', abbreviation: 'WA'},
+			{ name: 'WEST VIRGINIA', abbreviation: 'WV'},
+			{ name: 'WISCONSIN', abbreviation: 'WI'},
+			{ name: 'WYOMING', abbreviation: 'WY' }
+		];
 
     }
 
@@ -3793,7 +3852,7 @@
                 //		-redirect to dashboard
                 if (data.records['error'] != 'Token Timeout' && data.records['error'] != "Invalid Token") {
                     $scope.loggedIn = true;
-
+                    
                     //Store the user information
                     var info = {};
                     info.SessionID = cookieSessionID;
@@ -3803,6 +3862,9 @@
                     info.First = data.records.first_name;
                     info.Last = data.records.last_name;
                     info.Title = data.records.Title;
+                    $scope.name = data.records.first_name + " " + data.records.last_name;
+                    $scope.title = $cookieStore.get('Title');
+                    console.log("in here"+userInfo.get().Title);
                     info.UserLevelID = data.records.UserLevelID;
                     $scope.userLevel = data.records.UserLevelID;
                     userInfo.set(info);
@@ -3865,6 +3927,7 @@
                         userInfo.set(data.data.records);
                         $scope.name = data.data.records.First + " " + data.data.records.Last;
                         $scope.title = data.data.records.Title;
+                        $cookieStore.put('Title', $scope.title);
                         $scope.userLevel = data.data.records.UserLevelID;
                         $scope.loggedIn = true;
                         $window.location.href = "#/dashboard";
